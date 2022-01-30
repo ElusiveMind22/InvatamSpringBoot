cabfc61663e939e4ea95499ad0fca593eb50f68f
ElusiveMind22 <alberto-ionut.toscariu@student.tuiasi.ro>
2022-01-30 11:15:11 +0200

    now crud

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,9 +1,7 @@
 package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -19,8 +17,30 @@ public class StudentController {
     {
         this.studentService=studentService;
     }
+
     @GetMapping
     public List<Student> getStudents(){
         return studentService.getStudents();
     }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student)
+    {
+        studentService.addNewStudent(student);
+    }
+    @DeleteMapping(path = "{studentID}")
+    public void deleteStudent(@PathVariable("studentID") Long studentID){
+        studentService.deleteStudent(studentID);
+    }
+
+    @PutMapping(path = "{studentID}")
+    public void updateStudent(
+            @PathVariable("studentID") Long studentID,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email)
+    {
+        studentService.updateStudent(studentID,name,email);
+    }
+
+
 }
