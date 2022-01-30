88b585f8446691d256a92b0aff9866333915fddb
ElusiveMind22 <alberto-ionut.toscariu@student.tuiasi.ro>
2022-01-30 02:21:56 +0200

    added JPA support

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,8 +1,23 @@
 package com.example.demo.student;
 
+import javax.persistence.*;
 import java.time.LocalDate;
-
+@Entity (name = "Student")
+@Table(name = "Student",
+        uniqueConstraints = {
+        @UniqueConstraint(name="student_email_unique",columnNames = "email")
+})
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name="student_sequence",
+            sequenceName="student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private Integer age;
